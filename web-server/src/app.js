ec9d1d94ee66b01dabdb7dab99f6b59b210152ec
Juan Chavarro <chavarro@google.com>
2020-02-09 18:40:54 -0500

    add heroku support

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -7,6 +7,8 @@ const forecast = require("./utils/forecast");
 
 const app = express();
 
+const port = process.env.PORT || 5465;
+
 // Define paths for express config
 const publicDirectoryPath = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, "../templates/views");
@@ -83,6 +85,6 @@ app.get("*", (req, res) => {
   res.send("404 page");
 });
 
-app.listen(5465, () => {
-  console.log("Server is up on port 5465");
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
